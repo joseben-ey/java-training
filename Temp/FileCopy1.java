3289637d6c6835e1740f58eac4b8714080f097aa
joseben-ey <161955416+joseben-ey@users.noreply.github.com>
2024-03-07 17:07:19 +0530

    program2

--- Temp/FileCopy1.java
+++ Temp/FileCopy1.java
@@ -1,57 +1,58 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-public class CopyingFiles {
+public class ConferenceRoomBookingSystem {
+
+    private static final ConferenceRoom conferenceRoom = new ConferenceRoom();
 
     public static void main(String[] args) {
-        Thread fileCopy1Thread = new Thread(new FileCopy1());
-        Thread fileCopy2Thread = new Thread(new FileCopy2());
+        Thread manager1 = new Thread(new ConferenceRoomBookingJob(conferenceRoom, "Manager 1"));
+        Thread manager2 = new Thread(new ConferenceRoomBookingJob(conferenceRoom, "Manager 2"));
 
-        fileCopy1Thread.start();
-        fileCopy2Thread.start();
+        manager1.start();
+        manager2.start();
+    }
+}
 
+class ConferenceRoom {
+    private boolean isAvailable = true;
+
+    public synchronized boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public synchronized void bookRoom(String managerName) throws InterruptedException {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println(managerName + " booked the conference room.");
+            conductMeeting(managerName);
+            isAvailable = true;
+        } else {
+            System.out.println(managerName + " could not book the conference room - already occupied.");
+        }
+    }
+
+    private void conductMeeting(String managerName) throws InterruptedException {
+        System.out.println(managerName + " is conducting a meeting.");
+        TimeUnit.SECONDS.sleep(5); // Simulate meeting duration
+        System.out.println(managerName + " finished the meeting.");
+    }
+}
+
+class ConferenceRoomBookingJob implements Runnable {
+
+    private final ConferenceRoom conferenceRoom;
+    private final String managerName;
+
+    public ConferenceRoomBookingJob(ConferenceRoom conferenceRoom, String managerName) {
+        this.conferenceRoom = conferenceRoom;
+        this.managerName = managerName;
+    }
+
+    @Override
+    public void run() {
         try {
-            fileCopy1Thread.join();
-            fileCopy2Thread.join();
+            conferenceRoom.bookRoom(managerName);
         } catch (InterruptedException e) {
-            System.out.println("Interrupted while waiting for threads to finish.");
-            e.printStackTrace();
-        }
-
-        System.out.println("File copying completed (without ExecutorService).");
-    }
-}
-
-class FileCopy1 implements Runnable {
-    @Override
-    public void run() {
-        try (FileInputStream in = new FileInputStream("image1.bmp");
-             FileOutputStream out = new FileOutputStream("temp.bmp")) {
-            int b;
-            while ((b = in.read()) != -1) {
-                out.write(b);
-            }
-            System.out.println("FileCopy1: Copying complete.");
-        } catch (IOException e) {
-            System.out.println("FileCopy1: Error copying file.");
-            e.printStackTrace();
-        }
-    }
-}
-
-class FileCopy2 implements Runnable {
-    @Override
-    public void run() {
-        try (FileInputStream in = new FileInputStream("temp.bmp");
-             FileOutputStream out = new FileOutputStream("image2.bmp")) {
-            int b;
-            while ((b = in.read()) != -1) {
-                out.write(b);
-            }
-            System.out.println("FileCopy2: Copying complete.");
-        } catch (IOException e) {
-            System.out.println("FileCopy2: Error copying file.");
             e.printStackTrace();
         }
     }
