7c5e32953dbb3ccb0c0d7c7d8641f1e3f9782769
joseben-ey <161955416+joseben-ey@users.noreply.github.com>
2024-03-07 17:51:24 +0530

    program4

--- Temp/FileCopy1.java
+++ Temp/FileCopy1.java
@@ -1,59 +1,15 @@
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
-public class ConferenceRoomBookingSystem {
-
-    private static final ConferenceRoom conferenceRoom = new ConferenceRoom();
+public class Advisor {
+    private static final String[] advices = {
+            "Never begin to stop and never stop to begin.",
+            "Only the destination isn't important, one should enjoy the journey.",
+            "Impossible itself says 'I'm possible'"
+    };
 
     public static void main(String[] args) {
-        Thread manager1 = new Thread(new ConferenceRoomBookingJob(conferenceRoom, "Manager 1"));
-        Thread manager2 = new Thread(new ConferenceRoomBookingJob(conferenceRoom, "Manager 2"));
-
-        manager1.start();
-        manager2.start();
-    }
-}
-
-class ConferenceRoom {
-    private boolean isAvailable = true;
-
-    public synchronized boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public synchronized void bookRoom(String managerName) throws InterruptedException {
-        if (isAvailable) {
-            isAvailable = false;
-            System.out.println(managerName + " booked the conference room.");
-            conductMeeting(managerName);
-            isAvailable = true;
-        } else {
-            System.out.println(managerName + " could not book the conference room - already occupied.");
-        }
-    }
-
-    private void conductMeeting(String managerName) throws InterruptedException {
-        System.out.println(managerName + " is conducting a meeting.");
-        TimeUnit.SECONDS.sleep(5); // Simulate meeting duration
-        System.out.println(managerName + " finished the meeting.");
-    }
-}
-
-class ConferenceRoomBookingJob implements Runnable {
-
-    private final ConferenceRoom conferenceRoom;
-    private final String managerName;
-
-    public ConferenceRoomBookingJob(ConferenceRoom conferenceRoom, String managerName) {
-        this.conferenceRoom = conferenceRoom;
-        this.managerName = managerName;
-    }
-
-    @Override
-    public void run() {
-        try {
-            conferenceRoom.bookRoom(managerName);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Random random = new Random();
+        int randomIndex = random.nextInt(advices.length);
+        System.out.println("Free advice: " + advices[randomIndex]);
     }
 }
